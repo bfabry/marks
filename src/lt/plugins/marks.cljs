baf0f1a47fe8dc498d184ea6ef0d2295a173032b
Beau Fabry <bfabry@zendesk.com>
2014-02-21 11:09:54 +1100

    remember scroll positions for marks

--- src/lt/plugins/marks.cljs
+++ src/lt/plugins/marks.cljs
@@ -5,27 +5,37 @@
             [lt.objs.command :as cmd])
   (:require-macros [lt.macros :refer [behavior]]))
 
+(defn current-posn [editor]
+  {:cursor
+   (editor/->cursor editor)
+   :scroll (js->clj (.getScrollInfo (editor/->cm-ed editor)))})
+
 (behavior ::remember-edits
           :triggers #{:change}
           :reaction (fn [editor]
-                      (object/update! editor [::marks :last-edit-mark] #(identity (editor/->cursor editor)))))
+                      (object/update! editor [::marks :last-edit-mark] #(current-posn editor))))
 
 (behavior ::remember-large-movements
           :triggers #{:move}
           :reaction (fn [editor]
                       (object/update! editor [::marks]
                                       (fn [marks]
-                                        (let [previous-cursor (:current-cursor marks)
-                                              new-cursor (editor/->cursor editor)
+                                        (let [previous-posn (:current-posn marks)
+                                              previous-cursor (:cursor previous-posn)
+                                              new-posn (current-posn editor)
+                                              new-cursor (:cursor new-posn)
                                               distance (js/Math.abs (- (:line previous-cursor) (:line new-cursor)))]
                                           (if (> distance 5)
-                                            (merge marks {:current-cursor new-cursor, :last-large-movement-mark previous-cursor})
-                                            (merge marks {:current-cursor new-cursor})))))))
+                                            (merge marks {:current-posn new-posn, :last-large-movement-mark previous-posn})
+                                            (merge marks {:current-posn new-posn})))))))
 
 (behavior ::jump-to-mark
            :triggers #{::jump}
            :reaction (fn [editor mark]
-                       (editor/move-cursor editor (or (-> @editor ::marks mark) (editor/->cursor editor)))))
+                       (if-let [mark-posn (-> @editor ::marks mark)]
+                         (do
+                           (editor/move-cursor editor (:cursor mark-posn))
+                           (editor/scroll-to editor (get-in mark-posn [:scroll "left"]) (get-in mark-posn [:scroll "top"]))))))
 
 (cmd/command {:command ::jump-to-large-move-mark
               :desc "Marks: Jump to mark from last movement"
